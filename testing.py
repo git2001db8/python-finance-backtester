94b8d3cb5026605b19ce7447e605dfab21743bad
git2001db8 <katashi@SierraPro.local>
2020-11-12 15:53:24 +0900

    Update

---
+++ testing.py
@@ -0,0 +1,38 @@
+import os
+import pandas
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+specific = ['AMD']
+
+for filename in os.listdir('python-finance/datasets/'):
+    # print(filename)
+    symbol = filename.split('.')[0]
+    print(symbol)
+    if symbol in specific:
+        df = pandas.read_csv('python-finance/datasets/{}'.format(filename), parse_dates=True, index_col=0)
+
+print(df)
+df_ohlc = df['Adj Close'].resample('1W').ohlc()
+df_volume = df['Volume'].resample('1W').sum()
+df_resample = df_ohlc.join(df_volume)
+df_resample.reset_index(level=0, inplace=True)
+print(df_resample)
+
+trace1 = go.Candlestick(x=df_resample['Date'], open=df_resample['open'], high=df_resample['high'], low=df_resample['low'], close=df_resample['close'])
+trace2 = go.Bar(x=df_resample['Date'], y=df_resample['Volume'], yaxis="y2")
+data = [trace1, trace2]
+layout = go.Layout(
+    yaxis2=dict(
+        domain=[0, 0.2]
+    ),
+    legend=dict(
+        traceorder="reversed"
+    ),
+    yaxis=dict(
+        domain=[0.25, 1]
+    )
+)
+fig = go.Figure(data=data, layout=layout)
+fig.update(layout_xaxis_rangeslider_visible=False)
+fig.show()
