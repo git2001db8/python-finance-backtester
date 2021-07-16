8d6c0851cd9163dec8b6b9b4cba22975a67d5d0e
Kons <katashi.rain@gmail.com>
2021-07-16 18:48:52 +0900

    Combined Date and TIme

--- tradelog.py
+++ tradelog.py
@@ -2,8 +2,9 @@ import os
 import io
 import pandas as pd
 
-df_init = pd.read_csv('python-finance/trades/U6277264_20210713.tlg', sep='|', header=None, skiprows=5, engine='python', skipfooter=1)
-df_drop = df_init.drop(columns=[0,1,3,4,5,9,11,13,15])
-df_final = df_drop.rename(columns={2:'Symb', 6:'Code', 7:'Date', 8:'Time', 10:'Shares', 12:'Price', 14:'Comm'})
-print(df_final)
+df_init = pd.read_csv('python-finance/trades/U6277264_20210712.tlg', sep='|', header=None, skiprows=5, engine='python', skipfooter=1, parse_dates=[7, 8])
+df_drop = df_init.drop(columns=[0,1,3,4,5,7,9,11,13,15])
+#print(df_drop.head())
+df_final = df_drop.rename(columns={2:'Symb', 6:'Code', 8:'Date Time', 10:'Shares', 12:'Price', 14:'Comm'})
+print(df_final.head(4))
 #df_final.to_csv('Trade.csv')
