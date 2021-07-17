28cd2e9edcd33da9acffb1c311b89a6d4c5d14fb
Kons <katashi.rain@gmail.com>
2021-07-17 11:30:03 +0900

    New DF to store the shares for each trade

--- tradelog.py
+++ tradelog.py
@@ -6,5 +6,14 @@ df_init = pd.read_csv('python-finance/trades/U6277264_20210712.tlg', sep='|', he
 df_drop = df_init.drop(columns=[0,1,3,4,5,7,9,11,13,15])
 #print(df_drop.head())
 df_final = df_drop.rename(columns={2:'Symb', 6:'Code', 8:'Date Time', 10:'Shares', 12:'Price', 14:'Comm'})
-print(df_final.head(4))
+#print(df_final.head(4))
 #df_final.to_csv('Trade.csv')
+
+# New DF to store the shares for each trade
+df_open_trades = pd.DataFrame(columns=['Symbol', 'Open Shares', 'Total Shares', 'Status'])
+print(df_open_trades)
+
+
+# Loop for counting shares
+#for index in df_final.index:
+#   print(df_final['Shares'][index])
